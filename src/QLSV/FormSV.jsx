c9bbda31d9c91fd6a389ddce28aa1926ab9bb502
Trung <trung.nt.e@gmail.com>
2023-02-03 01:50:56 +0700

    Add component

--- src/QLSV/FormSV.jsx
+++ src/QLSV/FormSV.jsx
@@ -1,9 +1,178 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class FormSV extends Component {
+class FormSV extends Component {
+
+  state = {
+    values: {
+      maSV: "",
+      hoTen: "",
+      sdt: "",
+      email: ""
+    },
+    errors: {
+      maSV: "",
+      hoTen: "",
+      sdt: "",
+      email: ""
+    }
+  }
+
+  handleOnChange = (event) => {
+    console.log("Form: event.target: ", event.target);
+    let {value, name} = event.target;
+
+    let newValues = {...this.state.values};
+    newValues[name] = value;
+
+    let typeform = event.target.getAttribute("typeform");
+    console.log("Form: typeform: ", typeform);
+
+    let msgError = "";
+    if (value.trim() === ""){
+      msgError = `${name} không được để trống`;
+    }
+    //! kiem tra email
+    let regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (typeform === "email") {
+      // neu la email
+      if (!regexp.test(value)) {
+        // value khong dung voi dinh dang regexp
+        msgError = `Email không đúng định dạng`;
+      }
+    }
+
+    let newErrors = {...this.state.errors};
+    newErrors[name] = msgError;
+
+    this.setState({
+      values: newValues,
+      errors: newErrors
+    }, () => {
+      console.log("Form: SetState: ", this.state);
+    });
+
+  }
+
+
+  handleOnSubmit = (event) => {
+    // ! Ngan load lai trang web khi submit
+    event.preventDefault();
+    // console.log("handleOnSubmit:");
+    let isValid = true;
+    //! kiem tra errors con chua noi dung nao loi khong?
+    for (const property in this.state.errors) {
+      if (this.state.errors[property] !== "") {
+        // thuoc tinh con bi loi
+        isValid = false;
+        console.log("Error: ", property); // ten thuoc tinh
+      }
+    
+    }
+
+    //! kiem tra du lieu rong khi user khong doi gia tri, khong chay onChange
+    for (const property in this.state.values) {
+      if (this.state.values[property] === "") {
+        // thuoc tinh rong
+        isValid = false;
+        console.log("property: ", property);
+      }
+    }
+
+    if (isValid) {
+      // khong con loi -> day values (Nguoi dung moi) len redux
+      // console.log("Hop le");
+      let action = {
+        type: "THEM_SV",
+        sv: this.state.values
+      }
+      this.props.dispatch(action);
+    }
+    else {
+      alert("Form không được để trống");
+    }
+  }
+
+
+  //!Chỉ chạy khi props đổi
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      values: newProps.svChiTiet
+    });
+  }
+
+
   render() {
+    console.log("Form:", this.props);
+
+    let {maSV, hoTen, sdt, email} = this.state.values;
+
+
     return (
-      <div>FormSV</div>
+      <div className="row">
+          <div className="col-12">
+            <h2 className='bg-dark text-white p-2'>Thông tin sinh viên</h2>
+
+            <form  onSubmit={this.handleOnSubmit}>
+              <div className="row py-3">
+                <div className="col">
+                  <label htmlFor="maSV">Mã SV</label>
+                  <input 
+                    value={maSV}
+                    onChange={(event) => {
+                      this.handleOnChange(event);
+                    }}
+                    type="text" className="form-control" name="maSV" />
+                  <p className='text-danger'>{this.state.errors.maSV}</p>
+
+                </div>
+
+                <div className="col">
+                  <label htmlFor="hoTen">Họ tên</label>
+                  <input 
+                    value={hoTen}
+                    onChange={this.handleOnChange}
+                    type="text" className="form-control" name="hoTen" />
+                  <p className='text-danger'>{this.state.errors.hoTen}</p>
+
+                </div>
+              </div>
+
+              <div className="row py-3">
+                <div className="col">
+                  <label htmlFor="sdt">Số điện thoại</label>
+                  <input 
+                    value={sdt}
+                    onChange={this.handleOnChange}
+                    type="text" className="form-control" name="sdt" />
+                  <p className='text-danger'>{this.state.errors.sdt}</p>
+
+                </div>
+                <div className="col">
+                  <label htmlFor="email">Email</label>
+                  <input 
+                    value={email}
+                    onChange={this.handleOnChange}
+                    typeform="email" type="text" className="form-control" name="email" />
+                  <p className='text-danger'>{this.state.errors.email}</p>
+
+                </div>
+              </div>
+
+              <button className="btn btn-success">Thêm thành viên</button>
+            </form>
+
+
+          </div>
+        </div>
     )
   }
 }
+
+const mapStateToProps = (rootReducer) => {
+  return {
+    svChiTiet: rootReducer.QLSVReducer.svChiTiet
+  }
+}
+
+export default connect(mapStateToProps)(FormSV);
--- src/QLSV/TableSV.jsx
+++ src/QLSV/TableSV.jsx
@@ -1,9 +1,68 @@
 import React, { Component } from 'react'
 
-export default class TableSV extends Component {
+import { connect } from 'react-redux';
+class TableSV extends Component {
+
+  renderTable = () => {
+    return this.props.mangSV.map((sv) => {
+      return <tr key={sv.maSV}>
+                <td>{sv.maSV}</td>
+                <td>{sv.hoTen}</td>
+                <td>{sv.sdt}</td>
+                <td>{sv.email}</td>
+                <td>
+                  <button onClick={() => {
+                    let action = {
+                      type: "XEM_CHI_TIET",
+                      svXem: sv
+                    }
+                    this.props.dispatch(action);
+                  }} className="btn btn-success">Sửa</button>
+                  <button onClick={() => {
+                    let action = {
+                      type: "XOA_SV",
+                      maSVXoa: sv.maSV
+                    }
+                    this.props.dispatch(action);
+                  }} className="btn btn-danger">Xoá</button>
+                </td>
+              </tr>
+    });
+  }
+
+
   render() {
     return (
-      <div>TableSV</div>
+      <div className="row my-5">
+        <div className="col-12">
+
+          <table className="table">
+            <thead className='bg-dark text-white p-2'>
+              <tr>
+                <th scope="col">Mã SV</th>
+                <th scope="col">Họ tên</th>
+                <th scope="col">Số điện thoại</th>
+                <th scope="col">Email</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.renderTable()}
+            </tbody>
+          </table>
+
+
+        </div>
+      </div>
     )
   }
 }
+
+
+const mapStateToProps = (rootReducer) => {
+  return {
+    mangSV: rootReducer.QLSVReducer.mangSV
+  }
+}
+
+export default connect(mapStateToProps)(TableSV);
